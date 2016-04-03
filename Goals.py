77406a72a1db26e36370e2cdad718853348e13ff
Noel Yap <noel.yap+github.com@gmail.com>
2016-04-02 23:01:54 -0700

    Goals added.

---
+++ Goals.py
@@ -0,0 +1,32 @@
+# Rationale:
+# * Learn computational thinking
+#   * thought processes involved in formulating problems and their solutions
+#     so that the solutions are represented in a form
+#     that can be effectively carried out by an information-processing agent.
+#   * compositional reasoning, pattern matching, procedural thinking, recursive thinking
+#   * break down problems into smaller ones
+#   * abstract
+#     * power to scale and deal with complexity
+#   * consider failure conditions and scenarios
+#   * generalize solutions
+# * form hypotheses
+# * experiment
+# * Be more productive, efficient, and effective
+# * Computers are as much a part of today's life as is reading
+#
+# Introduce elementary school children to:
+# * the concepts of programming (eg structural, object-oriented, functional)
+# * good programming practices (eg TDD, DRY)
+# * divide-and-conquer to solve problems
+# * view problems in different ways (eg think of what can go wrong, break, etc)
+
+# How do we achieve success?
+# * Through having fun
+# * the maximal level of performance for individuals in a given domain
+#   is not attained automatically as a function of extended experience,
+#   but the level of performance can be increased
+#   even by highly experienced individuals
+#   as a result of deliberate efforts to improve.
+# * the most effective learning requires a well-defined task
+#   with an appropriate difficulty level for the particular individual,
+#   informative feedback, and opportunities for repetition and corrections of errors.
---
+++ course-1:basic-building-blocks/Goals.py
@@ -0,0 +1,5 @@
+# Input/Output
+# Primitive types (eg integers, strings, booleans)
+# Variables
+# Conditionals
+# Loops
