b4fc52e39d8ab598de669f7d30dee3c28888cf64
Noel Yap <noel.yap+github.com@gmail.com>
2016-04-03 23:12:08 -0700

    Folders and files renamed to make them more indicative of purpose.

---
+++ course-2:combining-building-blocks/subject-1:functions/topic-4:Functions as values/lesson-2:Fibonacci example.py
@@ -0,0 +1,8 @@
+sqrt = lambda n: n ** .5
+sqrt_5 = sqrt(5)
+phi = (1 + sqrt_5) / 2
+psi = (1 - sqrt_5) / 2
+fibonacci = lambda n: (phi ** n - psi ** n) / sqrt_5
+
+for n in range(-12, 12):
+	print(fibonacci(n))
