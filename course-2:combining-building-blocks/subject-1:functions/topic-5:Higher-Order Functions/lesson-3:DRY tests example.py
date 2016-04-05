da74ba18115f111dc7bc9b4eebdd5d895a4fd56c
Noel Yap <noel.yap+github.com@gmail.com>
2016-04-04 21:11:00 -0700

    DRY tests made DRYer.

--- course-2:combining-building-blocks/subject-1:functions/topic-5:Higher-Order Functions/lesson-3:DRY tests example.py
+++ course-2:combining-building-blocks/subject-1:functions/topic-5:Higher-Order Functions/lesson-3:DRY tests example.py
@@ -20,31 +20,10 @@ def test(condition):
 def is_equal_to(lhs, rhs):
 	return lambda: lhs == rhs
 
-def test_first():
-	test(is_equal_to(fibonacci(0), 0))
-
-def test_second():
-	test(is_equal_to(fibonacci(1), 1))
-
-def test_third():
-	test(is_equal_to(fibonacci(2), 1))
-
-def test_fourth():
-	test(is_equal_to(fibonacci(3), 2))
-
-def test_fifth():
-	test(is_equal_to(fibonacci(4), 3))
-
-def test_sixth():
-	test(is_equal_to(fibonacci(5), 5))
-
-def test_seventh():
-	test(is_equal_to(fibonacci(6), 8))
-
-test_first()
-test_second()
-test_third()
-test_fourth()
-test_fifth()
-test_sixth()
-test_seventh()
+test(is_equal_to(fibonacci(0), 0))
+test(is_equal_to(fibonacci(1), 1))
+test(is_equal_to(fibonacci(2), 1))
+test(is_equal_to(fibonacci(3), 2))
+test(is_equal_to(fibonacci(4), 3))
+test(is_equal_to(fibonacci(5), 5))
+test(is_equal_to(fibonacci(6), 8))
