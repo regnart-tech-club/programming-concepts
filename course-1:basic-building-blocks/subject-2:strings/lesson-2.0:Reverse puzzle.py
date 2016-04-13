05cc7a605deab24eb19fbe16e51b915e36a0d307
Noel Yap <noel.yap+github.com@gmail.com>
2016-04-12 21:46:17 -0700

    String lessons added.

---
+++ course-1:basic-building-blocks/subject-2:strings/lesson-2.0:Reverse puzzle.py
@@ -0,0 +1,2 @@
+# Write a program that uses input to assign your name to a variable
+# then outputs your name backwards.
---
+++ course-1:basic-building-blocks/subject-2:strings/lesson-2.1:Reverse solution.py
@@ -0,0 +1,6 @@
+# Write a program that assigns your name to a variable
+# then outputs your name backwards.
+name = 'Noel'
+
+print(name[3] + name[2] + name[1] + name[0])
+print((name[3] + name[2] + name[1] + name[0]).capitalize())
