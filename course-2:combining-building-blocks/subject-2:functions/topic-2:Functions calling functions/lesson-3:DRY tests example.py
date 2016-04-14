78171359c5a947cd76afbf3a3d1f99d9bd9a1a0d
Noel Yap <noel.yap+github.com@gmail.com>
2016-04-14 10:31:54 -0700

    DRY tests example added.

---
+++ course-2:combining-building-blocks/subject-2:functions/topic-2:Functions calling functions/lesson-3:DRY tests example.py
@@ -0,0 +1,14 @@
+# Write a function, `square`, that has one parameter and returns its square.
+def square(n):
+	return n ** 2
+
+def test_square_equals(arg, expected):
+	if square(arg) == expected:
+		print('Thumbs up.')
+	else:
+		print('Thumbs down.')
+
+test_square_equals(0, 0)
+test_square_equals(1, 1)
+test_square_equals(2, 4)
+test_square_equals(3, 9)
