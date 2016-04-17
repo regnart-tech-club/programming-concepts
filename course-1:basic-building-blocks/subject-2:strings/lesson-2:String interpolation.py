37f8bfb38a3a36c694ec15c793455b9c378badfa
Noel Yap <noel.yap+github.com@gmail.com>
2016-04-17 08:55:11 -0700

    String interpolation lesson added.

---
+++ course-1:basic-building-blocks/subject-2:strings/lesson-2:String interpolation.py
@@ -0,0 +1,20 @@
+aoeu = 'Hello'
+
+print(aoeu[0]) # H
+print(aoeu[1]) # e
+print(aoeu[4]) # o
+print(aoeu[823873838328])
+
+print(aoeu.index('H'))
+print(aoeu.index('e'))
+print(aoeu.index('o'))
+
+print('-----')
+
+print(2 * aoeu)
+
+print('-----')
+
+print(aoeu.capitalize())
+print(aoeu.lower())
+print(aoeu.upper())
