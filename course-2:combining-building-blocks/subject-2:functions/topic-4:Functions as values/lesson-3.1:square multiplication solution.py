93c6be1fdc50d5cc179dae84365ba67e5c82034a
Noel Yap <noel.yap+github.com@gmail.com>
2016-04-21 19:11:19 -0700

    Tests made DRY.

--- course-2:combining-building-blocks/subject-2:functions/topic-4:Functions as values/lesson-3.1:square multiplication solution.py
+++ course-2:combining-building-blocks/subject-2:functions/topic-4:Functions as values/lesson-3.1:square multiplication solution.py
@@ -1,31 +1,13 @@
 # Write a lambda, `square`, that has one parameter and returns its square.
 square = lambda n: n * n
 
-def test_0():
-	if square(0) == 0:
+def test_square_equals(arg, expected):
+	if square(arg) == expected:
 		print('Thumbs up.')
 	else:
 		print('Thumbs down.')
 
-def test_1():
-	if square(1) == 1:
-		print('Thumbs up.')
-	else:
-		print('Thumbs down.')
-
-def test_2():
-	if square(2) == 4:
-		print('Thumbs up.')
-	else:
-		print('Thumbs down.')
-
-def test_3():
-	if square(3) == 9:
-		print('Thumbs up.')
-	else:
-		print('Thumbs down.')
-
-test_0()
-test_1()
-test_2()
-test_3()
+test_square_equals(0, 0)
+test_square_equals(1, 1)
+test_square_equals(2, 4)
+test_square_equals(3, 9)
