58f2174129c23d0a3f1f6e1f5f2638aa2470d820
Noel Yap <noel.yap+github.com@gmail.com>
2016-04-23 07:39:57 -0700

    Description of Fibonacci sequence added.

--- course-2:combining-building-blocks/subject-2:functions/topic-1:Defining and calling functions/lesson-8.1:Fibonacci tmp var solution.py
+++ course-2:combining-building-blocks/subject-2:functions/topic-1:Defining and calling functions/lesson-8.1:Fibonacci tmp var solution.py
@@ -1,3 +1,6 @@
+# The Fibonacci sequence starts with 0 and 1. Subsequent terms are then gotten by adding the previous two
+# such that the first seven terms are: 0, 1, 1, 2, 3, 5, 8.
+
 # Write a function that, given an integer n, returns the nth Fibonacci number.
 # For example:
 # given n = 0, the function should return 0
--- course-2:combining-building-blocks/subject-2:functions/topic-3:Functions calling themselves/lesson-3.0:Fibonacci puzzle.py
+++ course-2:combining-building-blocks/subject-2:functions/topic-3:Functions calling themselves/lesson-3.0:Fibonacci puzzle.py
@@ -1,2 +1,10 @@
-# Using recursion, write a function that, given an integer n, returns the nth Fibonacci number.
-# For example, given n = 5, it should return 3.
+# The Fibonacci sequence starts with 0 and 1. Subsequent terms are then gotten by adding the previous two
+# such that the first seven terms are: 0, 1, 1, 2, 3, 5, 8.
+
+# Write a function that, given an integer n, returns the nth Fibonacci number.
+# For example:
+# given n = 0, the function should return 0
+# if n = 1, it should return 1
+# if n = 3, it should return 2
+# if n = 5, it should return 5
+# if n = 7, it should return 13
