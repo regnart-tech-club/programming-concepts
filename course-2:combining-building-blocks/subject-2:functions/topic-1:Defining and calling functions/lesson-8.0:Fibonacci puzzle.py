67dc0b6e03d147c25270e7c895e2b72885ede740
Noel Yap <noel.yap+github.com@gmail.com>
2016-04-22 21:42:57 -0700

    More examples given in puzzle description.

--- course-2:combining-building-blocks/subject-2:functions/topic-1:Defining and calling functions/lesson-8.0:Fibonacci puzzle.py
+++ course-2:combining-building-blocks/subject-2:functions/topic-1:Defining and calling functions/lesson-8.0:Fibonacci puzzle.py
@@ -1,2 +1,7 @@
 # Write a function that, given an integer n, returns the nth Fibonacci number.
-# For example, given n = 5, it should return 3.
+# For example:
+# given n = 0, the function should return 0
+# if n = 1, it should return 1
+# if n = 3, it should return 2
+# if n = 5, it should return 5
+# if n = 7, it should return 13
--- course-2:combining-building-blocks/subject-2:functions/topic-1:Defining and calling functions/lesson-8.2:Fibonacci swap solution.py
+++ course-2:combining-building-blocks/subject-2:functions/topic-1:Defining and calling functions/lesson-8.2:Fibonacci swap solution.py
@@ -1,3 +1,10 @@
+# Write a function that, given an integer n, returns the nth Fibonacci number.
+# For example:
+# given n = 0, the function should return 0
+# if n = 1, it should return 1
+# if n = 3, it should return 2
+# if n = 5, it should return 5
+# if n = 7, it should return 13
 def fibonacci(n):
   # Define two variables to hold interim values and initialize them.
   f0 = 0
