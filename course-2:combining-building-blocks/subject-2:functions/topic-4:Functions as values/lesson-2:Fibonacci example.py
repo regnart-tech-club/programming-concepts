e9e69c96282bc88a88e94101c0b6a3e16bfc8f9d
Noel Yap <noel.yap+github.com@gmail.com>
2016-04-24 08:50:17 -0700

    Fibonacci lambda implementation fixed.

--- course-2:combining-building-blocks/subject-2:functions/topic-4:Functions as values/lesson-2:Fibonacci example.py
+++ course-2:combining-building-blocks/subject-2:functions/topic-4:Functions as values/lesson-2:Fibonacci example.py
@@ -2,7 +2,7 @@ sqrt = lambda n: n ** .5
 sqrt_5 = sqrt(5)
 phi = (1 + sqrt_5) / 2
 psi = (1 - sqrt_5) / 2
-fibonacci = lambda n: (phi ** n - psi ** n) / sqrt_5
+fibonacci = lambda n: int(round((phi ** n - psi ** n) / sqrt_5))
 
 for n in range(-12, 12):
 	print(fibonacci(n))
